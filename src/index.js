ee915f52e51af45853129490be7b874ec32fb3a8
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2023-05-26 22:02:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+
+import "./styles.css";
+
+var a = "abc";
+var b = 1;
+var c = true;
+var d = null;
+var e;
+var f = undefined; // usually never do
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+// Null is a primitive data type of type Object,
+// It implifies the type of no value
+console.log(typeof e);
+console.log(typeof typeof f);
+// Everything JS is object
+//  Type of followed by n type of is string
+// Js will add ; for you
+// == , ===
+console.log(1 == 1);
+console.log(1 === "1");
+//Coersion or type conversion
+//
